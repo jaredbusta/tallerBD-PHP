80cf6dbbede97a8642aa452063b50f3e146c8d97
Oscar-M10 <oscar_medellin18@outlook.es>
2021-11-09 16:46:33 -0700

    v1

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -1,1 +1,12 @@
 <?php
+	//////////////// CONEXION A LA BD ////////////////////
+	$host="localhost";
+	$usuario="root";
+	$contrasena="";
+	$base="datos";
+	$conexion=new mysqli($host,$usuario,$contrasena,$base);
+	if($conexion -> connect_errno)
+	{
+		die("Fallo la conexion:(".$conexion -> mysqli_connect_errno().")".$conexion->mysqli_connect_error());
+	}
+?>
