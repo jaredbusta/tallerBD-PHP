fec88f752429cc787a2daab8130cc3592476cb22
Jared Bustamante <jaredbusta@gmail.com>
2021-11-09 22:59:45 -0700

    Archivo movido a nuevo directorio. Contiene la consulta que trae los usuarios de la tabla usuarios

---
+++ controller/usuarios/lista_usuarios.php
@@ -0,0 +1,11 @@
+<?php
+require "../../conexion/conexion.php";
+/////////////// CONSULTA A LA BD ///////////////////
+#$usuarios = "SELECT * FROM usuario";
+$usuarios = "SELECT * FROM  usuario order by id_usuario";
+$resUsuarios = $conexion->query($usuarios);
+$lista_usuarios=[];
+while ($registroUsuarios = $resUsuarios->fetch_array(MYSQLI_BOTH)) {
+    $lista_usuarios[] = $registroUsuarios;
+}
+echo json_encode($lista_usuarios);
